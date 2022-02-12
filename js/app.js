a84b13fc754c2ee89ea4811553542267e3f57f84
Mazharul <miforbd@gmail.com>
2022-02-12 21:13:06 +0600

    parameter name change x to show

--- js/app.js
+++ js/app.js
@@ -2,9 +2,9 @@ document.getElementById('clear').addEventListener('click',function(){
     const clear=document.getElementById('output');
     clear.value='';
 })
-function calc(x){
+function calc(show){
     const output=document.getElementById('output');
-    output.value=output.value+x;
+    output.value=output.value+show;
 }
 function equal(){
     const output=document.getElementById('output');
